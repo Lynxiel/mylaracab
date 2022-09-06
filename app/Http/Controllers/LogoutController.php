3048e499588ed045becadaf716f86009e43038c6
lynxiel <linansk@inbox.ru>
2022-09-06 09:08:23 +0300

    сессия сохраняется после выхода исправлен баг с отрицательным инпутом количества

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -16,8 +16,8 @@ class LogoutController extends Controller
     public function perform(Request $request)
     {
         Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+//        $request->session()->invalidate();
+//        $request->session()->regenerateToken();
         return redirect('/');
     }
 }
