18db2b4638909ca8d2b99bf9cae4b9c146e9903e
lynxiel <linansk@inbox.ru>
2022-09-15 12:36:31 +0300

    исправлен косяк для заказов от незареганных пользователей

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -79,9 +79,9 @@
                                                     <div  class="col-lg-6 col-sm-12 col-xs-12 col-md-6 ">
                                                         <form method="post" action="{{route('updateOrder')}}" >
                                                             @csrf
+                                                            <input type="text" hidden name="order_id" readonly value="{{$order->order_id}}">
                                                             @if (!empty($order->email))
                                                             <div class="row">
-                                                                <input type="text" hidden name="order_id" readonly value="{{$order->order_id}}">
 
                                                                 <div class="form-floating  col-lg-4 col-md-4 col-sm-6 col-xs-12">
                                                                     <input type="text" name="contact_name" disabled class="form-control rounded-3" id="name" value="{{$order->contact_name?:old('contact_name') }}">
