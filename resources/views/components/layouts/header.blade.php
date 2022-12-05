aaddddfb53851630244a237c79dfbc5b49defc1a
Elektrika <linansk@inbox.ru>
2022-12-05 11:21:31 +0300

    фавикон

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="{{$description}}"/>
     <title>{{ isset($title)?$title:$_ENV['APP_NAME'] }}</title>
+    <link rel="icon" href="{{ url('images/favicon.png') }}">
     <link rel="stylesheet" href="{{asset('css/bootstrap/dist/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.min.css')}}">
     <script src="{{asset('/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
