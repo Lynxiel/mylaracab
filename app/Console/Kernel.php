2e22fc65ad7819553520576bc8acfe1f156b22bf
Elektrika <linansk@inbox.ru>
2023-01-16 09:27:35 +0300

    обмен 1 раз в день

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     {
 
         $schedule->exec('/opt/php/8.1/bin/php /var/www/u1707254/data/www/kabelopt71.ru/artisan db:seed CableSeeder')
-            ->everyThreeHours()
+            ->daily()
             ->emailOutputTo(config('mail.reply_to.address'))
             ->sendOutputTo(base_path('logs/exchange.text'));
 
